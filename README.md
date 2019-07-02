9a1d3f75fda76394dff28c41d05173f547102a20
Aranđel Šarenac <arandjel@kahunahub.com>
2019-07-02 11:14:54 +0200

    refactor(var): add examples placeholder, postbuild, readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# Geo IP lookup service
+
+(Not ready for production yet)
+
+HTTP API to look up geo information by IP. Uses [maxmind](http://maxmind.com).
+
+## Project
+
+This service:
+
+- run `npm install` followed by `npm start`,
+- (install will take a bit depending on your internet connection, downloads a file ~55-60Mb on clean install)
+- if you provide MAXMIND_LICENSE_KEY, postbuild script will update the database with the latest one from maxmind servers (TODO)
+- if all goes well server should be listening @ `localhost:3000` or whatever port you provide via env vars
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/big-kahuna-burger/geo-ip-service)
+
+## Documentation
+
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
+- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
--- src/download-utils.js
+++ src/download-utils.js
@@ -1,10 +1,8 @@
-var hyperquest = require('hyperquest')
-var path = require('path')
-var concat = require('concat-stream')
-var tarfs = require('tar-fs')
-var zlib = require('zlib')
-
-const debug = require('debug')('bkb:geoip')
+const hyperquest = require('hyperquest')
+const path = require('path')
+const concat = require('concat-stream')
+const tarfs = require('tar-fs')
+const zlib = require('zlib')
 
 const { defaultUrl, vendorDir } = require('./constants')
 
@@ -21,24 +19,23 @@ function lastModified () {
 function download () {
   return new Promise((resolve, reject) => {
     hyperquest(defaultUrl)
-      .on('response', onResponse)
+      .on('response', res => {
+        return res
+          .pipe(zlib.createGunzip())
+          .pipe(
+            tarfs.extract(vendorDir, {
+              ignore: name => path.extname(name) !== '.mmdb',
+              map: header => {
+                // debug({ header })
+                header.name = header.name.split('/')[1]
+                return header
+              },
+              mapStream
+            })
+          )
+          .on('error', reject)
+      })
       .on('error', reject)
-    function onResponse (res) {
-      return res
-        .pipe(zlib.createGunzip())
-        .pipe(tarfs.extract(vendorDir, { ignore, map, mapStream }))
-        .on('error', reject)
-    }
-
-    function ignore (name) {
-      return path.extname(name) !== '.mmdb'
-    }
-
-    function map (header) {
-      // debug({ header })
-      header.name = header.name.split('/')[1]
-      return header
-    }
 
     function mapStream (fileStream, header) {
       if (path.extname(header.name) === '.mmdb') {
